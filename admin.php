1b56da8be208a70e0992f502119a42b7405340b9
Cheran Wu <cheranwu@gmail.com>
2013-12-05 15:15:25 -0500

    Make home pa ge

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Admin</title>
+</head>
+<body id="page3">
+<div class="main">
+	<div>
+		<span> To add an airport click <a href = "/db/AddAirport.php">here</a></span>
+	</div>
+	<div>
+		<span> To add a flightleg click <a href="/db/AddLeg.php">here</a></span>
+	</div>
+	<div>
+		<span> To add a plane click <a href = "/db/AddPlane.php">here</a></span>
+	</div>
+	<div>
+		<span> Admins who wish to edit the DB click <a href="/db/Delete.php">here</a></span>
+	</div>
+	<div>
+		<span> Customer who wish to book a flight click <a href = "/db/Switch.php">here</a></span>
+	</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+</head>
+<body id="page3">
+<div class="main">
+	<div>
+		<span> Customer who wish to book a flight click <a href = "/db/book.php">here</a></span>
+	</div>
+	<div>
+		<span> Admins who wish to edit the DB click <a href="/db/admin.php">here</a></span>
+	</div>
+</div>
+</body>
+</html>
